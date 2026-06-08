0cef61f026715a7c61d996f09e1e63f232a0d44c
whovipulsharma <vipul0019sh@gmail.com>
2026-06-08 19:59:39 +0530

    Day 8

---
+++ Week 1/Day 8/Q1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number of lines: ");
+int n=sc.nextInt();
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+        System.out.print("* ");
+    }
+    System.out.println("");
+    
+}
+}
+}
---
+++ Week 1/Day 8/Q3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q3
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number of lines: ");
+int n=sc.nextInt();
+for (int i = 1; i <= n; i++) {
+    for (int j = 65; (j-64) <= i; j++) {
+        System.out.print((char)j+" ");
+    }
+    System.out.println("");
+    
+}
+}
+}
